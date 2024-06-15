cd209f9c76edbf1efd31c068bc66a38775a4e0be
DevikaRajesh22 <devikaraj699@gmail.com>
2024-06-15 21:30:51 +0530

    links added

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -35,7 +35,7 @@ const RecentProjects = () => {
                                 ))}
                             </div>
                             <div className='flex justify-center items-center'>
-                                <p className='flex lg:text-xl md:text-xs text-sm text-purple'>Check live site</p>
+                                <a href={link}><p className='flex lg:text-xl md:text-xs text-sm text-purple'>Check live site</p></a>
                                 <FaLocationArrow className='ms-3' color='#CBACF9'/>
                             </div>
                         </div>
